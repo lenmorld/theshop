83e32b4d7e775603a70253d9fa63a6afcf3cd27b
Lenny LD <lenmorld@gmail.com>
2020-12-30 22:21:34 -0500

    fake login - query Customer.login

--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,53 @@
-import React from "react"
+import React, { useState } from "react"
 import Router from "next/router"
 
-// TODO: improve auth mechanism
+import requestGraphQL from "../lib/graphql"
+
 export default function Login() {
-  const login = () => {
-    localStorage.setItem("userId", "lenmorld")
-    Router.push("/secret")
+  const [login, setLogin] = useState()
+  const [password, setPassword] = useState()
+
+  const onSubmit = async () => {
+    const data = await requestGraphQL(
+      `query($login: String!) {
+          customer(where: { login: $login }) {
+            id
+            name
+            login
+          }
+        }
+      `,
+      {
+        login,
+      },
+    )
+
+    console.log(data)
+
+    if (data.customer) {
+      localStorage.setItem("userId", "lenmorld")
+      Router.push("/secret")
+    } else {
+      console.error("LOGIN FAILED")
+    }
   }
 
   return (
     <div>
       <h2>LOGIN COMPONENT</h2>
-      <button onClick={login}>LOGIN</button>
+      <label>
+        Login
+        <input type="text" onChange={(e) => setLogin(e.target.value)} />
+      </label>
+      <label>
+        Password
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button onClick={onSubmit}>LOGIN</button>
+      <div>
+        {login}
+        {password}
+      </div>
     </div>
   )
 }
