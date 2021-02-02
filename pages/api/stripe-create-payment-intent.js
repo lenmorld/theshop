3a5ff280cf0a85129dd7757d02c4c8a7740f9943
Lenny LD <lenmorld@gmail.com>
2021-02-01 20:13:17 -0500

    Integrate Stripe part 1 - create payment intent - backend: serverless function stripe-create-payment-intent - UI: createPaymentIntent fetch request

--- pages/api/stripe-create-payment-intent.js
+++ pages/api/stripe-create-payment-intent.js
@@ -1,27 +1,35 @@
 // const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const STRIPE_SECRET_KEY =
-  "sk_test_51I5EroGEcqGP7rtoa48iD8bQxNO7yJBkQtrwbhwdSbfT70j3ZGRtU0uZETq0TkrPXy6bXS0r4JRRsuczvX0ubArO00M9GFywDC"
+	'sk_test_51I5EroGEcqGP7rtoa48iD8bQxNO7yJBkQtrwbhwdSbfT70j3ZGRtU0uZETq0TkrPXy6bXS0r4JRRsuczvX0ubArO00M9GFywDC'
 
-const stripe = require("stripe")(STRIPE_SECRET_KEY)
+const stripe = require('stripe')(STRIPE_SECRET_KEY)
 
 export default async (req, res) => {
-  // const { body } = req
+	// const { body } = req
 
-  // https://stripe.com/docs/api/payment_intents/create
+	console.log(req.body)
 
-  const options = {
-    payment_method_types: ["card"],
-    amount: "1000",
-    currency: "CAD",
-  }
+	const { amount, currency } = req.body
 
-  try {
-    const paymentIntent = await stripe.paymentIntents.create(options)
-    res.json(paymentIntent)
-  } catch (err) {
-    res.json(err)
-  }
+	// https://stripe.com/docs/api/payment_intents/create
 
-  // res.statusCode = 200
-  // res.json({ name: "John Doe", body: req.body })
+	const options = {
+		payment_method_types: ['card'],
+		// amount: "1000",
+		// currency: "CAD",
+		// amount has to be integer
+		// https://stripe.com/docs/api/payment_intents/object#payment_intent_object-amount
+		amount: Math.ceil(amount),
+		currency,
+	}
+
+	try {
+		const paymentIntent = await stripe.paymentIntents.create(options)
+		res.json(paymentIntent)
+	} catch (err) {
+		res.json(err)
+	}
+
+	// res.statusCode = 200
+	// res.json({ name: "John Doe", body: req.body })
 }
