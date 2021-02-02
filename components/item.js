64851d8a06eb4bb25199b712a72c891332c587fe
Lenny LD <lenmorld@gmail.com>
2021-02-01 20:17:48 -0500

    ESLint and cleanup

--- components/item.js
+++ components/item.js
@@ -1,88 +1,47 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from 'react'
 
-import styles from "../styles/Home.module.css"
+import styles from '../styles/Item.module.css'
 
-import requestGraphQL from "../lib/graphql"
+import requestGraphQL from '../lib/graphql'
 
-export default function Item({ item: _item }) {
-  const [item, setItem] = useState(_item)
+import { formatCurrency } from '../lib/formats'
 
-  const sendMutation = async () => {
-    const data = await requestGraphQL(
-      `mutation {
-            updateItem(where: {
-              id: "${item.id}"
-            }, data: {
-              stock: ${item.stock - 1}
-            }) {
-              id
-              stock
-            }
-          }
-          `,
-    )
+export default function Item({ item, addToCart }) {
+	// const [item, setItem] = useState(_item)
 
-    // console.log(`graphql result:`, data)
+	// console.log('item: ', item)
 
-    // PUBLISH
-    await requestGraphQL(
-      `mutation {
-            publishItem(where: {
-              id: "${item.id}"
-            }, to:PUBLISHED) {
-              id,
-              stock
-            }
-          }
-          `,
-    )
-
-    console.log(data.updateItem)
-
-    setItem({
-      ...item,
-      ...data.updateItem,
-    })
-    // return {
-    //   props: {
-    //     items: data.updateItem,
-    //   },
-    // }
-  }
-
-  return (
-    <div className={styles.card}>
-      <h3>{item.name}</h3>
-      <div style={{ margin: "1rem 0" }}>
-$
-{item.price}
-      </div>
-      <img
-        alt={item.name}
-        src={item.imageUrl}
-        style={{
-          width: "200px",
-          height: "200px",
-          objectFit: "cover",
-        }}
-      />
-      <p>{item.description}</p>
-      <div
-        style={{
-          margin: "1rem 0",
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        {/* <button className={styles.buttonPrimary}>Add to cart</button> */}
-        <button className="buttonPrimary" onClick={sendMutation}>
-          Test button
-        </button>
-        <div style={{ display: "inline-block" }}>
-          <div style={{ fontWeight: "bold" }}>Stock</div>
-          <div>{item.stock}</div>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className={styles.item}>
+			<h3>{item.name}</h3>
+			<img
+				alt={item.name}
+				src={item.imageUrl}
+				style={{
+					width: '200px',
+					height: '200px',
+					objectFit: 'cover',
+				}}
+			/>
+			<p>{item.description}</p>
+			<div
+				style={{
+					margin: '1rem 0',
+					display: 'flex',
+					justifyContent: 'space-between',
+				}}
+			>
+				{/* <button className={styles.buttonPrimary}>Add to cart</button> */}
+				{/* <button className="buttonPrimary" onClick={sendMutation}> */}
+				<button className="buttonPrimary" onClick={addToCart}>
+					Add to cart
+				</button>
+				<div style={{ margin: '1rem 0' }}>{formatCurrency(item.price)}</div>
+				{/* <div style={{ display: 'inline-block' }}>
+					<div style={{ fontWeight: 'bold' }}>Stock</div>
+					<div>{item.stock}</div>
+				</div> */}
+			</div>
+		</div>
+	)
 }
