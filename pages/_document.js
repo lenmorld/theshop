3e3cd004b855203e82726d9f6bba20f4f43d572e
Lenny LD <lenmorld@gmail.com>
2021-02-01 19:56:45 -0500

    Customize document to add portal for Modal

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+/**
+ * https://nextjs.org/docs/advanced-features/custom-document
+ */
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx)
+		return { ...initialProps }
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head />
+				<body>
+					<Main />
+					<NextScript />
+					<div id="portal" />
+				</body>
+			</Html>
+		)
+	}
+}
+
+export default MyDocument
